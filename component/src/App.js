9b3c3d5e204457c11a297b386769f5752e918df9
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-25 12:21:32 +0900

    props rendering

--- component/src/App.js
+++ component/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  return <MyComponent name="React" />;
 };
 
 export default App;
--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -1,38 +1,5 @@
-const MyComponent = () => { //화살표 함수는 자신이 종속된 인스턴스를 가리킴
-    return <div>나의 새롭고 멋진 컴포넌트</div>
-}
+const MyComponent = props => {
+    return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
+};
+
 export default MyComponent;
-
-
-  //화샇표함수와 일반함수의 비교
-  //일반함수
-  //일반함수는 자신이 종속된 객체를 this로 가리킴
-/*function BlackDog() {
-  this.name = '흰둥이';
-  return {
-    name: '검둥이',
-    bark: function () {
-      console.log(this.name + ':멍멍!');
-    }
-  }
-}
-const blackDog = new BlackDog();
-blackDog.bark();
-*/
-
-  //화살표함수
-  //화샇표함수는 자신이 종속된 인스턴스를 가리킴
-/*
-function WhiteDog(){
-  this.name = '흰둥이';
-  return {
-    name : '검둥이';
-    bark : () => {
-      console.log(this.name + ':멍멍!'); //흰둥이: 멍멍!
-    }
-  }
-}
-const whiteDog = new WhiteDog();
-whiteDog.bark();
-*/
-
