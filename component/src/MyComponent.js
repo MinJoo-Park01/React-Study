8cd0a0253a6c0839a2fba7fd9979f364fa7ba8be
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-27 22:28:44 +0900

    destructuring assignment

--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -1,10 +1,14 @@
-const MyComponent = props => {
-    return <div>안녕하세요, 제 이름은 {props.name}입니다.<br />
+const MyComponent = ({ name, children }) => {
+    //const { name, children } = props;
+    return (<div>안녕하세요, 제 이름은 {name}입니다.<br />
         {/*br태그는 줄바꿈*/}
-    children 값은 {props.children}
+    children 값은 {children}
     입니다.
-    </div>;
+    </div>
+    );
 };
+//비구조화 할당 문법을 통해 props 추출
+//전에 props.name이라고 표현하지 않고, name으로 표현
 
 MyComponent.defaultProps = {
     name: '기본이름'
