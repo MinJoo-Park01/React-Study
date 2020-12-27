819d23378bc76de20bb1e797c256344faef08068
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-27 22:57:06 +0900

    functional component proptypes

--- component/src/App.js
+++ component/src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent>리액트</MyComponent>;
+  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
 };
 //태그 사이에 있는 내용을 보여주는 children
+//숫자로 나타내면, 표시가 되긴하네,,,?
+//콘솔창에 오류가 뜨긴함
 export default App;
--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -1,9 +1,14 @@
-const MyComponent = ({ name, children }) => {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MyComponent = ({ name, children, favoriteNumber }) => {
     //const { name, children } = props;
     return (<div>안녕하세요, 제 이름은 {name}입니다.<br />
         {/*br태그는 줄바꿈*/}
     children 값은 {children}
     입니다.
+        <br />
+    제가 좋아하는 숫자는 {favoriteNumber}입니다.
     </div>
     );
 };
@@ -14,5 +19,10 @@ MyComponent.defaultProps = {
     name: '기본이름'
 };
 //props를 따로 설정하지 않았을 때 보여주는 기본값
+
+MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+}; //name값을 string형태로 전달
 
 export default MyComponent;
