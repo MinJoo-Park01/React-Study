6ba60dd00baf58803545af27c638cad8acc9f246
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-27 17:12:13 +0900

    default props

--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -2,4 +2,9 @@ const MyComponent = props => {
     return <div>안녕하세요, 제 이름은 {props.name}입니다.</div>;
 };
 
+MyComponent.defaultProps = {
+    name: '기본이름'
+};
+//props를 따로 설정하지 않았을 때 보여주는 기본값
+
 export default MyComponent;
