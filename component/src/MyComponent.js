1f65ca8bf18276cbb0f4b2d8f5242855df30d9de
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-28 11:19:06 +0900

    Adding props in Class Component

--- component/src/MyComponent.js
+++ component/src/MyComponent.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, children, favoriteNumber }) => {
-    //const { name, children } = props;
-    return (<div>안녕하세요, 제 이름은 {name}입니다.<br />
-        {/*br태그는 줄바꿈*/}
-    children 값은 {children}
-    입니다.
-        <br />
-    제가 좋아하는 숫자는 {favoriteNumber}입니다.
-    </div>
-    );
-};
-//비구조화 할당 문법을 통해 props 추출
-//전에 props.name이라고 표현하지 않고, name으로 표현
+class MyComponent extends Component {
+    render() {
+        const { name, favoriteNumber, children } = this.props;
+        return (
+            <div>
+                안녕하세요. 제 이름은 {name}입니다.
+                children 값은 {children}
+                입니다.
+                <br />
+                제가 좋아하는 숫자는 {favoriteNumber}입니다.
+            </div>
+        );
+    }
+}
 
 MyComponent.defaultProps = {
     name: '기본이름'
