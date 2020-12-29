3fd19da75b167dd75ed0f01bfe1181cc7529fabd
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-29 22:52:38 +0900

    using constructor and binding

--- event/src/EventPractice.js
+++ event/src/EventPractice.js
@@ -5,6 +5,26 @@ class EventPractice extends Component {
     state = {
         message: ''
     }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleClick() {
+        alert(this.state.message);
+        this.setState({
+            message: ''
+        });
+    }
+
     render() {
         return (
             <div>
@@ -14,15 +34,11 @@ class EventPractice extends Component {
                     name="message"
                     placeholder="아무거나 입력해보세요"
                     value={this.state.message}
-                    onChange={
-                        (e) => {
-                            this.setState({
-                                message: e.target.value
-                            }) //e는 syntheticEvent, 웹 브라우저의 네이티브 이벤트를 감싸는 객체
-                            //e.target.value는 앞으로 변할 인풋값
-                        }
-                    }
+                    onChange={this.handleChange}
                 />
+                <button onClick={
+                    this.handleClick
+                }>확인</button>
             </div>
         );
     }
