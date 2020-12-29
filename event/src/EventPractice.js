a64b58fd4d8ea9a9c70cb246067a30291305a9df
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-29 14:16:46 +0900

    using e.target.value

--- event/src/EventPractice.js
+++ event/src/EventPractice.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
-
 class EventPractice extends Component {
     render() {
         return (
             <div>
                 <h1>이벤트 연습</h1>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder="아무거나 입력해보세요"
+                    onChange={
+                        (e) => {
+                            console.log(e.target.value); //e는 syntheticEvent, 웹 브라우저의 네이티브 이벤트를 감싸는 객체
+                            //e.target.value는 앞으로 변할 인풋값
+                        }
+                    }
+                />
             </div>
         );
     }
 }
 
-export default App;
+export default EventPractice;
