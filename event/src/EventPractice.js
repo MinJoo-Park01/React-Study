bf8150f70e8ff9835726cacf4df8d7dac5aa72f6
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-30 00:29:42 +0900

    form object

--- event/src/EventPractice.js
+++ event/src/EventPractice.js
@@ -3,15 +3,32 @@ import React, { useState } from 'react';
 //이벤트연습을 클래스형 컴포넌트가 아닌 함수형 컴포넌트로 나타냄
 
 const EventPractice = () => {
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const { username, message } = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form, //기존의 form내용을 이자리에 복사
+            [e.target.name]: e.target.value //원하는 값을 덮어 씌우기
+        };
+        setForm(nextForm);
+    };
+    /*
     const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
     const onChangeUsername = e => setUsername(e.target.value);
     const onChangeMessage = e => setMessage(e.target.value);
+    */
     const onClick = () => {
         alert(username + ' : ' + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username: '',
+            message: ''
+        });
     };
+
     const onKeyPress = e => {
         if (e.key === 'Enter') {
             onClick();
@@ -25,14 +42,14 @@ const EventPractice = () => {
                 name="username"
                 placeholder="사용자명 입력"
                 value={username}
-                onChange={onChangeUsername}
+                onChange={onChange}
             />
             <input
                 type="text"
                 name="messge"
                 placeholder="메시지를 입력하세요"
                 value={message}
-                onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyPress={onKeyPress}
             />
             <button onClick={onClick}>확인</button>
