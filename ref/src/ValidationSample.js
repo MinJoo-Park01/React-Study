6b38407ee29cd10425942002f89e34e7da13a155
MinJoo-Park01 <supermp@hufs.ac.kr>
2020-12-31 13:36:16 +0900

    example using DOM

---
+++ ref/src/ValidationSample.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './ValidationSample.css';
+
+class ValidationSample extends Component {
+    state = {
+        password: '',
+        clicked: false,
+        validated: false
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            password: e.target.value;
+        });
+    }
+
+    handleButtonClick = () => {
+        this.setState({
+            clicked: true,
+            validated: this.state.password === '0000'  //password가 0000일때 true로 반환하라는 건가?
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    type='password'
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                    className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}
+                />
+                <button onClick={this.handleButtonClick}>검증하기</button>
+            </div>
+        );
+    }
+}
+
+export default ValidationSample;
