4b92e19783f2a9c9321c04a6288d7a99c907b7e2
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-01 10:53:21 +0900

    add onKeyPress

--- ref/src/ValidationSample.js
+++ ref/src/ValidationSample.js
@@ -20,6 +20,11 @@ class ValidationSample extends Component {
             validated: this.state.password === '0000'  //password가 0000일때 true로 반환하라는 건가?
         })
     }
+    onKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            this.handleButtonClick();
+        }
+    }
 
     render() {
         return (
@@ -29,6 +34,7 @@ class ValidationSample extends Component {
                     value={this.state.password}
                     onChange={this.handleChange}
                     className={this.state.clicked ? (this.state.validated ? 'success' : 'failure') : ''}
+                    onKeyPress={this.onKeyPress}
                 />
                 <button onClick={this.handleButtonClick}>검증하기</button>
             </div>
