a5114448cfb7eecb5307857eccc0369290053095
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-01 21:31:25 +0900

    casting data array to component array

---
+++ iteration/src/IterationSample.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const IterationSample = () => {
+    /*
+    return (
+        <ul>
+            <li>눈사람</li>
+            <li>얼음</li>
+            <li>눈</li>
+            <li>바람</li>
+        </ul>
+
+    )
+    */
+    //데이터 배열을 컴포넌트 배열로 변환
+    const names = ['눈사람', '얼음', '눈', '바람'];
+    const nameList = names.map(name => <li>{name}</li>);
+    return <ul>{nameList}</ul>
+
+}
+export default IterationSample;
