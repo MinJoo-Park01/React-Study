1baa7c76a6212dfa12b9bd49c6115ab736c40bf4
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-04 23:29:11 +0900

    [add] custom Hooks

--- hooks/src/Info.js
+++ hooks/src/Info.js
@@ -1,45 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import useInputs from './useInputs';
 
 const Info = () => {
-    const [name, setName] = useState('');
-    const [nickname, setNickname] = useState('');
-
-    useEffect(() => {
-        //1. componentDidMount + ComponentDidUpdate
-        // console.log('렌더링이 완료되었습니다!!');
-        // console.log({
-        //     name,
-        //     nickname
-        // });
-        //2. Mount때만 실행
-        //두번째 파라미터로 비어있는 배열 넣어주기
-        //     console.log('마운트될때만 실행');
-        // },[]);
-        // 3. 특정값(예를 들어 이름만)이 업데이트 될때만 실행
-        //     console.log(name);
-        // }, [name]);
-        //4. 뒷정리하기
-        console.log('effect');
-        console.log(name);
-        return () => {
-            console.log('cleanup');
-            console.log(name);
-        };
+    const [state, onChange] = useInputs({
+        name: '',
+        nickname: ''
     });
+    //const [name, setName] = useState('');
+    //const [nickname, setNickname] = useState('');
 
-    const onChangeName = e => {
-        setName(e.target.value);
-    };
+    const { name, nickname } = state;
 
-    const onChangeNickname = e => {
-        setNickname(e.target.value);
-    };
+    // useEffect(() => {
+    //     //1. componentDidMount + ComponentDidUpdate
+    //     // console.log('렌더링이 완료되었습니다!!');
+    //     // console.log({
+    //     //     name,
+    //     //     nickname
+    //     // });
+    //     //2. Mount때만 실행
+    //     //두번째 파라미터로 비어있는 배열 넣어주기
+    //     //     console.log('마운트될때만 실행');
+    //     // },[]);
+    //     // 3. 특정값(예를 들어 이름만)이 업데이트 될때만 실행
+    //     //     console.log(name);
+    //     // }, [name]);
+    //     //4. 뒷정리하기
+    //     console.log('effect');
+    //     console.log(name);
+    //     return () => {
+    //         console.log('cleanup');
+    //         console.log(name);
+    //     };
+    // });
+
+    // const onChangeName = e => {
+    //     setName(e.target.value);
+    // };
+
+    // const onChangeNickname = e => {
+    //     setNickname(e.target.value);
+    // };
 
     return (
         <div>
             <div>
-                <input value={name} onChange={onChangeName} />
-                <input value={nickname} onChange={onChangeNickname} />
+                <input value={name} onChange={onChange} />
+                <input value={nickname} onChange={onChange} />
             </div>
             <div>
                 <div>
