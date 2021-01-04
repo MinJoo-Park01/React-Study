de99383e91fc993a8169458b49b58b851caf2038
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-04 21:12:30 +0900

    [add] useMemo Average

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
+import Average from './Average';
 import Counter from './Counter';
 import Info from './Info';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? '숨기기' : '보이기'}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  return <Average />
 };
 
 export default App;
