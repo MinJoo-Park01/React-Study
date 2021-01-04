2954061123cdc0f13bf1fb1d4b761f8dab4f7ed1
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-04 14:11:35 +0900

    [add] Info visible button

--- hooks/src/Info.js
+++ hooks/src/Info.js
@@ -1,8 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Info = () => {
     const [name, setName] = useState('');
     const [nickname, setNickname] = useState('');
+
+    useEffect(() => {
+        //1. componentDidMount + ComponentDidUpdate
+        // console.log('렌더링이 완료되었습니다!!');
+        // console.log({
+        //     name,
+        //     nickname
+        // });
+        //2. Mount때만 실행
+        //두번째 파라미터로 비어있는 배열 넣어주기
+        //     console.log('마운트될때만 실행');
+        // },[]);
+        // 3. 특정값(예를 들어 이름만)이 업데이트 될때만 실행
+        //     console.log(name);
+        // }, [name]);
+        //4. 뒷정리하기
+        console.log('effect');
+        console.log(name);
+        return () => {
+            console.log('cleanup');
+            console.log(name);
+        };
+    });
 
     const onChangeName = e => {
         setName(e.target.value);
