48b8e634f22f4069059c5cd5516aa8cce77f08c6
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-11 16:59:43 +0900

    [feat] todolist

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './TodoTemplate';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 투두리스트 만들기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '토플 오답노트',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: '블로그 포스팅',
+      checked: true,
+    },
+  ])
+
+  //고유값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; //nextId 1씩 더하기
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos]
+  )
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      )
+    },
+    [todos],
+  )
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
--- todo-app/src/TodoInsert.js
+++ todo-app/src/TodoInsert.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.css';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+        //console.log({ value });
+    }, []);
+
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue('');
+
+            e.preventDefault(); //submit 이벤트는 브라우저에서 새로고침을 발생하는데, 이를 막기위해서 이 함수를 호출함
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
-            <input placeholder="할 일 입력" />
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="할 일 입력"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd />
             </button>
