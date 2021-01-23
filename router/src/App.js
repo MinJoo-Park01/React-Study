088939ad113016fcf0b3b86ead51965846a820b7
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-23 12:47:44 +0900

    [feat] Not Found 404

--- router/src/App.js
+++ router/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profile from './profile';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
@@ -20,12 +21,26 @@ const App = () => {
         <li>
           <Link to="/profile/gildong">gildong 프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
-      <Route path="/" component={Home} exact={true} />
-      {/* <Route path="/about" component={About} />
-      <Route path="/info" component={About} /> */}
-      <Route path={['/about', '/info']} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        {/* <Route path="/about" component={About} />
+        <Route path="/info" component={About} /> */}
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profile/:username" component={Profile} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   )
 }
